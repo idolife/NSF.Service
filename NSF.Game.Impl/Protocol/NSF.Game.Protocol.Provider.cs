3ee58a71f9cc4e9676b89dd153276fa99ad98c4f
idolife <csb4game@163.com>
2014-05-19 22:16:39 +0800

    协议包的打包解�

--- NSF.Game.Impl/Protocol/NSF.Game.Protocol.Provider.cs
+++ NSF.Game.Impl/Protocol/NSF.Game.Protocol.Provider.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using ProtoBuf.Meta;
+using Newtonsoft.Json;
 using NSF.Share;
 using NSF.Interface;
 
@@ -13,6 +12,7 @@ namespace NSF.Game.Logic
     /// </summary>
     public static class ProtocollProvide
     {
+        public static Int32 MAX_PACKAGE_SIZE = 1024 * 8;
         //public static Type GetTypeById(Int32 msgId)
         //{
         //    throw new NotImplementedException();
@@ -28,15 +28,61 @@ namespace NSF.Game.Logic
         /// </summary>
         public static GameReq DecodeMessage(IDataBlock chunk)
         {
-            throw new NotImplementedException();
+            byte[] dataBuff = chunk.Buffer;
+            Int32 dataOffset = chunk.ReadPosition;
+            Int32 dataLength = chunk.Length;
+            const Int32 INT32SIZE = sizeof(Int32);
+
+            /// 不足包头的长度
+            if (dataLength < INT32SIZE)
+                return null;
+            Int32 msgHeadSign = BitConverter.ToInt32(dataBuff, dataOffset);
+            /// 低位4字节为完整包的长度
+            Int32 msgFullLength = msgHeadSign & 0x0000FFFF;
+            /// 不足完整包长度             
+            if (dataLength < msgFullLength)
+                return null;
+
+            /// 包足够一个完整包
+            dataLength -= INT32SIZE;
+            dataOffset += INT32SIZE;
+
+            /// 使用Protobuff解析获得消息包的对象
+            /// 反序列化消息
+            Int32 protoLength = msgFullLength - INT32SIZE;
+            MemoryStream readStream = new MemoryStream(dataBuff, dataOffset, protoLength);
+            GameReq msgObj = new GameReq();
+            RuntimeTypeModel.Default.Deserialize(readStream, msgObj, typeof(GameReq));
+            /// 移动数据块读指针
+            chunk.ReadOffset(msgFullLength);
+
+            return msgObj;
         }
 
         /// <summary>
         /// 协议打包。
         /// </summary>
-        public static Byte[] EncodeMessage(Object msgObj)
+        public static ArraySegment<Byte> EncodeMessage(Object msgCore)
         {
-            throw new NotImplementedException();
+            /// 准备缓存
+            const Int32 INT32SIZE = sizeof(Int32);
+            Byte[] msgFullBuff = new Byte[MAX_PACKAGE_SIZE];
+            MemoryStream msHeader = new MemoryStream(msgFullBuff, 0, INT32SIZE);
+
+            /// 打包Json消息
+            String msgJsonString = JsonConvert.SerializeObject(msgCore);
+            /// 序列化消息对象
+            MemoryStream msBody = new MemoryStream(msgFullBuff, INT32SIZE, MAX_PACKAGE_SIZE - INT32SIZE);
+            GameAck msgObj = new GameAck { Json = msgJsonString };
+            RuntimeTypeModel.Default.Serialize(msBody, msgObj);
+
+            /// 序列化消息头
+            BinaryWriter hdWriter = new BinaryWriter(msHeader);
+            hdWriter.Write((Int32)msBody.Position + INT32SIZE);
+
+            /// 返回打包后的缓存
+            return new 
+                ArraySegment<byte>(msgFullBuff, 0, (Int32)msBody.Position + INT32SIZE);
         }
     }
 }
